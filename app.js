ca3babe5e52588b9b18ca51f978f57cec02e147b
Nano Liklikadze <nanoliklikadze@Nanos-MacBook-Pro.local>
2022-11-30 01:20:00 -0500

    more changes

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ import HelloController from "./controller/hello-controller.js";
 import UserController from "./controller/users/users-controller.js";
 import TuitsController from "./controller/tuits/tuits-controller.js";
 
-const CONNECTION_STRING = "mongodb+srv://nanoLiklikadze:Snowbell@cluster0.c4upre4.mongodb.net/tuiter?retryWrites=true&w=majority"
-                          || "mongodb://localhost:27017/tuiter"
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+                          || 'mongodb://localhost:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
+
 
 const app = express()
 app.use(cors())   // import the new cors library
