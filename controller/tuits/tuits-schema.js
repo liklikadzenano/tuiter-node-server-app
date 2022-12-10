6d2e01c0ca7de30e34da902c98f413af03a14470
Nano Liklikadze <nanoliklikadze@Nanos-MacBook-Pro.local>
2022-12-10 01:02:24 -0500

    switched to render

--- controller/tuits/tuits-schema.js
+++ controller/tuits/tuits-schema.js
@@ -3,5 +3,7 @@ const schema = mongoose.Schema({
                                    tuit: String,
                                    likes: Number,
                                    liked: Boolean,
+
+
                                }, {collection: "tuits"}); // collection name where tuits are stored in tuiter database
 export default schema;
